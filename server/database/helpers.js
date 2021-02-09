73079ab8e374789158d8e5e3247821d97427dda4
John Sciutto <johnsciutto@gmail.com>
2021-02-09 05:14:35 -0500

    Create /server/database/helper.js and connectDB fn
    Created file to hold helper functions for database use, and created the
    connectDB function.

---
+++ server/database/helpers.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const {
+  DB_USER,
+  DB_PASS,
+  DB_NAME
+} = process.env;
+
+/**
+ * Opens a connection to the MongoDB Atlas instance used in this project, using
+ * the individual login credentials created in the root environment file.
+ * 
+ * @returns {Promise|object} If connection is successful, returns Promise that
+ * resolves to 'this'. If connection fails, returns object with a message and
+ * the error.
+ */
+export const connectDB = () => {
+  const URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.sd3mb.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+  const options = { useNewUrlParser: true, useUnifiedTopology: true }
+  return mongoose.connect(URI, options, (err) => {
+    if (err) {
+      return {
+        msg: 'Database connection failed.',
+        error: err
+      };
+    }
+  });
+}
