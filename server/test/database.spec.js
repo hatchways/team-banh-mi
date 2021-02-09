506f6f44c9d63271c5b61659a802e892889926c9
John Sciutto <johnsciutto@gmail.com>
2021-02-09 12:40:04 -0500

    Outline for database tests. Not implemented yet.

---
+++ server/test/database.spec.js
@@ -0,0 +1,8 @@
+const { connectDB, disconnectDB } = require('../database/helpers');
+
+describe('Database', () => {
+  describe('helpers.js', () => {
+    it('should throw an error when the connection fails', () => {});
+    it('should not throw an error when the connection succeeds', () => {});
+  });
+});
