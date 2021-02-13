89e6d922a9297335387100f89f205cca883cf7ce
John Sciutto <johnsciutto@gmail.com>
2021-02-13 05:42:18 -0500

    Apply prettier to loginController.js

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -3,27 +3,27 @@ const User = db.user;
 const UserData = require("../userModel/userData");
 const hp = require("../utils/hasProperty");
 
-function loginUser(field){
-    UserData.findDataByEmail(field.email,(result) => {
-        if(!hp.hasErrorProperty(result)){
-            return { "status": "500", result };
-        }
-    
-        console.log(result);
-        if(!hp.hasUserProperty(result)){
-            return { "status": "404", "error": "User Not found." };
-        }
+function loginUser(field) {
+  UserData.findDataByEmail(field.email, (result) => {
+    if (!hp.hasErrorProperty(result)) {
+      return { status: "500", result };
+    }
 
-        const password = User.isPasswordValid(field.password,user.password);
+    console.log(result);
+    if (!hp.hasUserProperty(result)) {
+      return { status: "404", error: "User Not found." };
+    }
 
-        if (!password) {
-            return { "status": "401", "error": "Invalid Password!" };
-        }
+    const password = User.isPasswordValid(field.password, user.password);
 
-        const token = UserData.generateAuthToken(result);
+    if (!password) {
+      return { status: "401", error: "Invalid Password!" };
+    }
 
-        return {"token":token};
-    });
+    const token = UserData.generateAuthToken(result);
+
+    return { token: token };
+  });
 }
 
 exports.loginUser = loginUser;
