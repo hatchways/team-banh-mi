ded43c0a2d889e1e2b7defe4b2768dee4207689a
shahgrishma123 <shahgrishma123@gmail.com>
2021-02-13 20:00:50 -0500

    Remove comments from app.js file

--- server/app.js
+++ server/app.js
@@ -23,13 +23,6 @@ connectDB("test");
 
 app.use("/", indexRouter);
 
-/*app.use(function(req, res, next) {
- res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, Content-Type, Accept"
-  );
-  next();
-});*/
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 
