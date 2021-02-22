d95dba8a7b704571cc2d92c908e1c7bb137b1528
John Sciutto <johnsciutto@gmail.com>
2021-02-22 17:46:13 -0500

    Create scafolding for settings page

---
+++ client/src/pages/Settings.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+}));
+
+function Settings() {
+  const styles = useStyles();
+  return (<div></div>);
+}
+
+export default Settings;
