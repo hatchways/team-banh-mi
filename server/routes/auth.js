b0847f7cc1c92bff8aad9b37a3f2f6f13af19da1
John Sciutto <johnsciutto@gmail.com>
2021-02-22 17:16:04 -0500

    Wrote first mock of logout route and controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,13 @@
 const registerController = require("../controllers/registerController");
 const loginController = require("../controllers/loginController");
+const logoutController = require("../controllers/logoutController");
 const express = require("express");
 const router = express.Router();
 
 router.post("/register", registerController.registerUser);
 
 router.post("/login", loginController.loginUser);
+
+router.post("/logout", logoutController.logoutUser);
 
 module.exports = router;
