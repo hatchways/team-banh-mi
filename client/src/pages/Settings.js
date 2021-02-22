04d497ecad7c747cdf0f328f33d9802b59f14ec5
John Sciutto <johnsciutto@gmail.com>
2021-02-22 17:55:53 -0500

    Moved Navbar to /src/components.

---
+++ client/src/pages/Settings.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+}));
+
+function Settings() {
+  const styles = useStyles();
+  return (<div></div>);
+}
+
+export default Settings;
