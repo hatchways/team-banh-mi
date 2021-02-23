bd1cbf591496f9e3b768301af9f6897e4e74e15c
HenryZou1 <henryzou2@hotmail.com>
2021-02-23 12:20:59 -0500

    Revert "reddit"
    This reverts commit 050fdf131b5c3ec506d1d0e5d89b6d62f3a44d51.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
-
 const createError = require("http-errors");
 const express = require("express");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-require("dotenv").config({ path: join(__dirname, ".env") });
-
+const cors = require("cors");
 const indexRouter = require("./routes/index");
-var snoowrap = require('snoowrap');
-const { createMention, connectDB } = require("./utils/database");
+const authRouter = require("./routes/auth");
+const { connectDB, disconnectDB } = require("./utils/database");
+const { corsOptions } = require("./middlewares/cors");
 
 const { json, urlencoded } = express;
 
@@ -17,50 +16,25 @@ const app = express();
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// TODO: Change this in production. Remove the argument.
+connectDB("test");
 
 app.use("/", indexRouter);
-const r = new snoowrap({
-  userAgent: 'webcrawler',
-  clientId: 'rwl4j4FrYnxqPA',
-  clientSecret: 'qZ1p1Cp8q2Va6gBv8A18oI2KZGrK0Q',
-  username: 'bot3424',
-  password: 'bot3424'
-});
-connectDB("test");
-redditSearch('burgerking');
-function redditSearch(query){
-  r.search({query: query,subreddit: 'all',sort: 'top'}).then((data) =>{
-  data.forEach(element=>{
-    date = new Date(element.created_utc*1000);
-     createMention(element.selftext, element.title, "reddit", media(element.media_embed),date, element.ups, element.permalink);
-    
-  }
-    )
-}
-)
-}
-
-function media(media){
-  if(!media)
-    return "none";
-  else if(media ="{}")
-    return "none"
-  else
-    return media
-}
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  
   next(createError(404));
-  
 });
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
@@ -70,4 +44,3 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
-
