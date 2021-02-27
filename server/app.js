5fa08d592b70691fcb0de919c5736353b280e61c
HenryZou1 <henryzou2@hotmail.com>
2021-02-27 17:19:55 -0500

    redditUpdate+SearchForComponent

--- server/app.js
+++ server/app.js
@@ -5,24 +5,32 @@ const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config({ path: join(__dirname, ".env") });
-
 const indexRouter = require("./routes/index");
-
 const { connectDB } = require("./utils/database");
-
 const { redditSearch,getReddit } = require("./utils/redditcrawler");
-
 const { json, urlencoded } = express;
-
 const app = express();
+const bull = require('bull');
+
+const searchQueue = new bull('searchQueue', {redis: {port: 6379, host:'127.0.0.1'}})
+searchQueue.process(function (job, done){
+  console.log("helloworld");
+  done();
+  return("hello");
+});
+
+searchQueue.add({}, {repeat: {cron: '*/1 * * * *'}});
+
+
+
 
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
-
 app.use("/", indexRouter);
+
 
 connectDB("test");
 
