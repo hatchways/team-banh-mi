742129257519113a4148dcf00b29b5f056c614d2
John Sciutto <johnsciutto@gmail.com>
2021-02-28 15:49:27 -0500

    Fix small contianer bug.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -14,8 +14,6 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
   },
   screenContainer: {
-    width: "100%",
-    height: "90vh",
     display: "flex",
   },
   mainScreen: {
