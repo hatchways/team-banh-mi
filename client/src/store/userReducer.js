9ddb76ea860b345bebdfb309dac7f37d3d73a23c
John Sciutto <johnsciutto@gmail.com>
2021-03-03 19:41:15 -0500

    Fixed reducer.

--- client/src/store/userReducer.js
+++ client/src/store/userReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import cloneDeep from "lodash.clonedeep";
 
 const userInitialState = {
   id: "",
@@ -6,12 +7,14 @@ const userInitialState = {
   companyName: [],
   platforms: [],
   favoriteMentions: [],
+  onlyFavorites: true,
 };
 
 const updateUserData = (state, user) => {
+  const clonedState = cloneDeep(state);
   return {
-    ...state,
-    id: user._id,
+    ...clonedState,
+    id: user.id,
     email: user.email,
     companyName: user.companyName,
     platforms: user.platforms,
@@ -19,37 +22,48 @@ const updateUserData = (state, user) => {
   };
 };
 
+const toggleOnlyFavorites = (state) => {
+  const clonedState = cloneDeep(state);
+  return { ...clonedState, onlyFavorites: !clonedState.onlyFavorites };
+};
+
 const addCompanyName = (state, action) => {
-  state.companyName.push(action.companyName);
-  return state;
+  const clonedState = cloneDeep(state);
+  clonedState.companyName.push(action.companyName);
+  return clonedState;
 };
 
 const deleteCompanyName = (state, action) => {
-  const index = state.companyName.indexOf(action.companyName);
-  state.companyName.splice(index, 1);
-  return state;
+  const clonedState = cloneDeep(state);
+  const index = clonedState.companyName.indexOf(action.companyName);
+  clonedState.companyName.splice(index, 1);
+  return clonedState;
 };
 
 const addFavoriteMention = (state, action) => {
-  state.favoriteMentions.push(action.favoriteMentions);
-  return state;
+  const clonedState = cloneDeep(state);
+  clonedState.favoriteMentions.push(action.favoriteMentions);
+  return clonedState;
 };
 
 const deleteFavoriteMention = (state, action) => {
-  const index = state.favoriteMentions.indexOf(action.favoriteMentions);
-  state.favoriteMentions.splice(index, 1);
-  return state;
+  const clonedState = cloneDeep(state);
+  const index = clonedState.favoriteMentions.indexOf(action.favoriteMentions);
+  clonedState.favoriteMentions.splice(index, 1);
+  return clonedState;
 };
 
 const addPlatform = (state, action) => {
-  state.platforms.push(action.platform);
-  return state;
+  const clonedState = cloneDeep(state);
+  clonedState.platforms.push(action.platform);
+  return clonedState;
 };
 
 const deletePlatform = (state, action) => {
-  const index = state.platforms.indexOf(action.platform);
-  state.platforms.splice(index, 1);
-  return state;
+  const clonedState = cloneDeep(state);
+  const index = clonedState.platforms.indexOf(action.platform);
+  clonedState.platforms.splice(index, 1);
+  return clonedState;
 };
 
 const userReducer = (state = userInitialState, action) => {
@@ -68,6 +82,8 @@ const userReducer = (state = userInitialState, action) => {
       return addPlatform(state, action);
     case actionTypes.DELETE_PLATFORM:
       return deletePlatform(state, action);
+    case actionTypes.TOGGLE_ONLY_FAVORITES:
+      return toggleOnlyFavorites(state);
     default:
       return state;
   }
