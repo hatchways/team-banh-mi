625165c01e22d1f4760aa277a485343cb82c15e0
John Sciutto <johnsciutto@gmail.com>
2021-03-04 11:55:15 -0500

    Create userReducer to manage favorite display.

--- client/src/store/userReducer.js
+++ client/src/store/userReducer.js
@@ -1,25 +1,19 @@
 import * as actionTypes from "./actionTypes";
 
 const initialState = {
-  email: null,
-  companyName: null,
-  platforms: null,
-  favoriteMentions: null,
+  onlyFavorites: false,
 };
 
-const updateUserData = ({
-  email,
-  companyName,
-  platforms,
-  favoriteMentions,
-}) => {
-  return { email, companyName, platforms, favoriteMentions };
-};
+const showOnlyFavorites = (state) => ({ ...state, onlyFavorites: true });
+
+const showAllMentions = (state) => ({ ...state, onlyFavorites: false });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.UPDATE_USER_DATA:
-      return updateUserData(action.user);
+    case actionTypes.SHOW_ONLY_FAVORITES:
+      return showOnlyFavorites(state);
+    case actionTypes.SHOW_ALL_MENTIONS:
+      return showAllMentions(state);
     default:
       return initialState;
   }
